c93c72afabd2a48e08ce84353b5021fa6e2df523
Jake Kravas <jakekravas7@gmail.com>
2020-12-02 12:46:25 -0700

    added ratings to home screen view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header/>
       <main className="py-3">
         <Container>
-          <HomeScreen/>
+          {/* using 'exact' below makes it so that only / will take us to homescreen, not / anything */}
+          <Route path="/" component={HomeScreen} exact/>
         </Container>
       </main>
       <Footer/>
-    </>
+    </Router>
   );
 }
 
