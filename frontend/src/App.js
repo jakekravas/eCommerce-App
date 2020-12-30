b2d281a0f4f9dcd10846dcee46e554c2525775d7
Jake Kravas <jakekravas7@gmail.com>
2020-12-30 14:06:21 -0700

    Setup cart screen functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
           {/* using 'exact' below makes it so that only / will take us to homescreen, not / anything */}
           <Route path="/" component={HomeScreen} exact/>
           <Route path="/product/:id" component={ProductScreen}/>
+          {/* Using ? after :id allow the id to be optional */}
+          <Route path="/cart/:id?" component={CartScreen}/>
         </Container>
       </main>
       <Footer/>
